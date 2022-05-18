21d939356d2e86407ae8fe5811bb9963624c7623
Iplay6432 <71528024+Iplay6432@users.noreply.github.com>
2022-05-18 18:18:40 -0400

    password finder :D

---
+++ main.py
@@ -0,0 +1,29 @@
+import random  
+import string
+import time
+print ("What Is The Password That We Will try to Find?\n" "No Special Charcters")
+x=input("")
+guess = ""
+tries = 0
+attempts = []
+def specific_string(length):  
+    sample_string = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890' # define the specific string  
+    # define the condition for random string  
+    global result
+    result = ''.join((random.choice(sample_string)) for x in range(length))
+start = time.time()
+while guess != x:
+    specific_string(len(x))
+    tries += 1
+    guess = result
+    if guess != x:
+        attempts.append(guess)
+end = time.time()
+final = end - start
+final = str(final)
+print(attempts)
+tries = str(tries)
+print(tries + " Tries")
+print("Your Password was " + result)
+print("It Took " + final + " Secconds")
+    
