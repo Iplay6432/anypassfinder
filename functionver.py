ec2718b04f8b0fe0b6d735df82c700575ce631eb
Iplay6432 <71528024+Iplay6432@users.noreply.github.com>
2022-05-18 18:22:51 -0400

    Create functionver.py

---
+++ functionver.py
@@ -0,0 +1,33 @@
+import random  
+import string
+import time
+def findpass(pas):
+    print ("What Is The Password That We Will try to Find?\n" "No Special Charcters")
+    guess = ""
+    tries = 0
+    attempts = []
+    #Random String Generaotr
+    def specific_string(length):  
+        sample_string = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890' # define the specific string  
+        # define the condition for random string  
+        global result
+        result = ''.join((random.choice(sample_string)) for x in range(length))
+    start = time.time()
+    #Checks If THe Pass Is Correct
+    while guess != pas:
+        specific_string(len(pas))
+        tries += 1
+        guess = result
+        if guess != pas:
+            attempts.append(guess)
+    end = time.time()
+    final = end - start
+    final = str(final)
+    print(attempts)
+    tries = str(tries)
+    print(tries + " Tries")
+    print("Your Password was " + result)
+    print("It Took " + final + " Secconds")
+#This is where you put the passowrd
+findpass("ad")
+        
